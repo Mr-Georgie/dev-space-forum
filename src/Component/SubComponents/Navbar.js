1a5f489accbb9251311229763ba67b5f2f4c6b6a
mr-georgie <george.isiguzo@yahoo.com>
2022-05-09 22:51:47 +0100

    search input update

--- src/Component/SubComponents/Navbar.js
+++ src/Component/SubComponents/Navbar.js
@@ -11,7 +11,7 @@ export default function Header() {
 
   const navigate = useNavigate()
 
-  const { logOut, user } = useContext(UserContext)
+  const { logOut, user, toast } = useContext(UserContext)
   const [navContent, setNavContent] = useState(false)
 
   const toggleNavContent = () => {
@@ -32,6 +32,10 @@ export default function Header() {
       
   })
 
+  const info = () => {
+    toast.info("Oops! Sorry.. This search form has not been implemented")
+  }
+
 
   return (
     <header className="fixed top-0 left-0 right-0 z-20 px-8 bg-white border-b-2 lg:px-16">
@@ -39,17 +43,32 @@ export default function Header() {
       {/* Navbar starts here */}
       <nav className="md:flex md:justify-between py-2">
         <div className="flex items-center">
-          <svg xmlns="http://www.w3.org/2000/svg" onClick={toggleNavContent} 
-            className="
-                h-6 w-6 cursor-pointer 
-                md:hidden block" 
-            fill="none" 
-            viewBox="0 0 24 24" 
-            stroke="currentColor" 
-            strokeWidth={2}
-          >
-            <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
-          </svg>
+          {
+            !navContent &&
+            <svg xmlns="http://www.w3.org/2000/svg" onClick={toggleNavContent} 
+              className="
+                  h-6 w-6 cursor-pointer 
+                  md:hidden block" 
+              fill="none" 
+              viewBox="0 0 24 24" 
+              stroke="currentColor" 
+              strokeWidth={2}
+            >
+              <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+            </svg>
+          }
+          {
+            navContent &&
+            <svg xmlns="http://www.w3.org/2000/svg" onClick={toggleNavContent} 
+              className="h-6 w-6" 
+              fill="none" 
+              viewBox="0 0 24 24" 
+              stroke="currentColor" 
+              strokeWidth={2}
+            >
+              <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+            </svg>
+          }
           <Link to="/home" className="nav-link p-2"> DevSpace</Link>{/* Put Logo here */}
         </div>
 
@@ -63,12 +82,15 @@ export default function Header() {
               </svg>
             </span>
             <input 
-              className="placeholder:italic placeholder:text-slate-400 block 
+              className="placeholder:italic placeholder:text-slate-400 block disabled:opacity-75 
                       bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 
                       shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" 
               placeholder="Search for a devspace..." 
               type="text" 
-              name="search"/>
+              name="search"
+              // disabled
+              onClick={info}  
+            />
           </label>
 
           <div className="flex flex-col items-end gap-3 md:flex-row md:items-center md:gap-5 md:static">
